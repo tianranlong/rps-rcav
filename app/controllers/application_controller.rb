805e36e1316bfd7278d1b9319ee308e1fb84cc59
tlong5 <tlong5@chicagobooth.edu>
2022-10-29 18:58:22 +0000

    commmmm

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,24 @@ def play_rock
   render({ :template => "game_templates/user_rock_html.erb"})
 end
 
+def play_paper
+  #redirect_to("https://www.google.com")
+
+  render({ :template => "game_templates/user_paper_html.erb"})
+end
+
+def play_scissors
+  #redirect_to("https://www.google.com")
+
+  render({ :template => "game_templates/user_scissors_html.erb"})
+end
+
+
+
+def homepage
+  #redirect_to("https://www.google.com")
+
+  render({ :template => "game_templates/rules.html.erb"})
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get("/rock", { :controller => "application", :action => "play_rock" })
+  get("/", { :controller => "application", :action => "homepage" })
+  get("/paper", { :controller => "application", :action => "play_paper" })
+  get("/scissors", { :controller => "application", :action => "play_scissors" })
 
 end
